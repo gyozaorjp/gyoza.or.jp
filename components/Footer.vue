47251b9a020725e3e93a7299244da90eafba9565
Chikara Onodera <chikara@shikake.com>
2022-12-20 11:25:21 +0900

    footerなぜ二重に？

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <footer class="p-4">
+  <section class="p-4">
     <div>
       <span class="footer-title">協会概要</span> 
       <NuxtLink class="link link-hover" to="/about/">焼き餃子協会について</NuxtLink> 
@@ -17,7 +17,6 @@
       <NuxtLink class="link link-hover" to="/statute/">定款</NuxtLink>
       <NuxtLink to="/transactionlaw/" class="link link-hover">特定商取引法に基づく表示</NuxtLink>
     </div>
-
     <div>
       <span class="footer-title">Social</span> 
       <div class="grid grid-flow-col gap-4">
@@ -25,5 +24,5 @@
         <a href="https://www.facebook.com/yaki.gyoza.japan/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
       </div>
     </div>
-  </footer>
+  </section>
 </template>
