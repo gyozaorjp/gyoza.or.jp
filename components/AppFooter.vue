b8f687ada1e2c01f8f763e8d4a09d2ecb7407242
Chikara Onodera <chikara@shikake.com>
2025-09-02 18:59:04 +0900

    Jオイルバナーサイズの調整

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -3,12 +3,12 @@
     <section class="bg-orange-50 p-4 sm:pb-8 my-4">
       <h2 class="text-center text-2xl sm:text-4xl font-bold">オフィシャルスポンサー</h2>
       <ul class="container mt-4 mx-auto flex flex-col sm:flex-row justify-center items-center gap-4">
-        <li>
+        <li class="w-full sm:w-1/3">
           <a href="https://www.j-oil.com/" target="_blank" class="block shadow-md hover:shadow-xl">
             <img src="/img/joil/joil-corp.png" alt="株式会社J-オイルミルズ" class="w-full">
           </a>
         </li>
-        <li>
+        <li class="w-full sm:w-1/3">
           <a href="https://www.foodservice-joyl.com/" target="_blank" class="block shadow-md hover:shadow-xl">
             <img src="/img/joil/joil-business.png" alt="業務用油脂サイト" class="w-full">
           </a>
